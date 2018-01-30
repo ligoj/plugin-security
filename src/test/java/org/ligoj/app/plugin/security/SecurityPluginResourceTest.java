72a18c438082014943d892bb16206c946482f26c
Fabrice Daugan <fabrice.daugan@gfi.fr>
2018-01-31 00:04:22 +0100

    Update API to 2.1.0

--- src/test/java/org/ligoj/app/plugin/security/SecurityPluginResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/security/SecurityPluginResourceTest.java
@@ -5,21 +5,21 @@ import java.nio.charset.StandardCharsets;
 
 import javax.transaction.Transactional;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.ligoj.app.AbstractAppTest;
 import org.ligoj.app.model.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Test class of {@link SecurityResource}
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
 @Rollback
 @Transactional
@@ -28,7 +28,7 @@ public class SecurityPluginResourceTest extends AbstractAppTest {
 	@Autowired
 	private SecurityResource resource;
 
-	@Before
+	@BeforeEach
 	public void prepareData() throws IOException {
 		persistEntities("csv", new Class[] { Node.class }, StandardCharsets.UTF_8.name());
 	}
@@ -36,6 +36,6 @@ public class SecurityPluginResourceTest extends AbstractAppTest {
 	@Test
 	public void getKey() {
 		// Coverage only
-		Assert.assertEquals("service:security", resource.getKey());
+		Assertions.assertEquals("service:security", resource.getKey());
 	}
 }
